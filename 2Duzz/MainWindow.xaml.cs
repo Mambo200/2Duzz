4152b994614abffba0f987bc43eebce84c1b4d5a
Tobias <tobis-stroedicke@gmx.de>
2021-06-14 16:12:30 +0200

    Change WrapPanel to Grid
    - LAYERS!
    - WrapPanel did not work out so good, so is god changed to "Grid" with Children which type is WrapPanel.
    - Add "ImageHelper" to add Images to all kind of Panels
    - Rename some MVVM Variables

--- 2Duzz/MainWindow.xaml.cs
+++ 2Duzz/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using _2Duzz.ViewModels;
+﻿using _2Duzz.Helper;
+using _2Duzz.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace _2Duzz
 {
     /// <summary>
@@ -23,80 +25,22 @@ namespace _2Duzz
     public partial class MainWindow : Window
     {
         public static string[] Args;
-        public MainViewModel GetMainViewModel { get => (MainViewModel)this.DataContext; }
+        public MainViewModel GetMainViewModel { get => this.DataContext as MainViewModel; }
         public MainWindow()
         {
             InitializeComponent();
 
             BindingHelper.Get.Init(this);
+            ImageHelper.Get.Init(this);
             ScollViewer_Images.MainW = this;
-            AddImageToPanel(new Uri("E:\\Tobias\\Bilder\\ebf5__150_player_emotes_by_kupogames-dbn7dy7\\emo0001.jpg"));
+            //Image i = ImageHelper.Get.AddImageToPanel(new Uri("E:\\Tobias\\Bilder\\ebf5__150_player_emotes_by_kupogames-dbn7dy7\\emo0001.jpg"), 2);
+            //ChangeStatusBar(((Panel)GridContent_Images.Children[2]).Children.IndexOf(i));
         }
 
-        /// <summary>
-        /// Add Image to Wrappanel
-        /// </summary>
-        /// <param name="_source">Uri source</param>
-        /// <returns>added Image</returns>
-        public Image AddImageToPanel(Uri _source)
-        {
-            // create BitmapImage for Image source
-            BitmapImage bImg = new BitmapImage();
-            bImg.BeginInit();
-            bImg.UriSource = _source;
-            bImg.EndInit();
-
-            // create Image for UI Visibility
-            Image img = new Image();
-            img.BeginInit();
-            img.Source = bImg;
-            img.Stretch = Stretch.Fill;
-            img.EndInit();
-            WrapPanel_Images.Children.Add(img);
-
-            // Set Binding
-            BindingOperations.SetBinding(img, Image.WidthProperty, BindingHelper.Get.BindingImageSizeWidth);
-            BindingOperations.SetBinding(img, Image.HeightProperty, BindingHelper.Get.BindingImageSizeHeight);
-
-            return img;
-        }
-
-        /// <summary>
-        /// Add Image to Wrappanel
-        /// </summary>
-        /// <param name="_source">Stream source. Stream does not get disposed automatically so you need to save stream and dispose if afterwards if you do not need in anymore!</param>
-        /// <returns>added Image</returns>
-        public Image AddImageToPanel(Stream _source)
-        {
-            // create BitmapImage for Image source
-            BitmapImage bImg = new BitmapImage();
-            bImg.BeginInit();
-            bImg.StreamSource = _source;
-            bImg.EndInit();
-
-            // create Image for UI Visibility
-            Image img = new Image();
-            img.BeginInit();
-            img.Source = bImg;
-            img.Stretch = Stretch.Fill;
-            img.EndInit();
-            WrapPanel_Images.Children.Add(img);
-
-            // Set Binding
-            BindingOperations.SetBinding(img, Image.WidthProperty, BindingHelper.Get.BindingImageSizeWidth);
-            BindingOperations.SetBinding(img, Image.HeightProperty, BindingHelper.Get.BindingImageSizeHeight);
-
-            return img;
-        }
 
         public void ChangeStatusBar(object _content)
         {
             GetMainViewModel.StatusBarContent = _content;
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
         }
 
         private void ImageClick(object sender, MouseButtonEventArgs e)
@@ -115,14 +59,14 @@ namespace _2Duzz
         {
             if (Keyboard.IsKeyDown(Key.LeftCtrl))
             {
-                GetMainViewModel.WPScale = Math.Max(0.1, GetMainViewModel.WPScale + e.Delta * 0.001);
+                GetMainViewModel.GridContentScale = Math.Max(0.1, GetMainViewModel.GridContentScale + e.Delta * 0.001);
             }
         }
 
         private void Zoom_MouseWheelWithoutCtrl(object sender, MouseWheelEventArgs e)
         {
             e.Handled = true;
-            GetMainViewModel.WPScale = Math.Max(0.01, GetMainViewModel.WPScale + e.Delta * 0.001);            
+            GetMainViewModel.GridContentScale = Math.Max(0.01, GetMainViewModel.GridContentScale + e.Delta * 0.001);            
         }
     }
 }
