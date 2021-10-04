8a08f191131d889781b65f2089efe2b1b346c300
Tobias Stroedicke <tobis-stroedicke@gmx.de>
2021-10-04 18:26:08 +0200

    Add Name of author from code in header

--- 2Duzz/Images/EzSplitBitmap.cs
+++ 2Duzz/Images/EzSplitBitmap.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿////////////////////////
+// Code von Robertico //
+////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
